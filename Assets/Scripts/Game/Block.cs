ce85866c0bf77135fcc4e0191adaac8f21bf0d7b
NullOne01 <NullOne01@users.noreply.github.com>
2022-10-09 06:58:00 +0300

    Added drag and drop on block/pocket

--- Assets/Scripts/Game/Block.cs
+++ Assets/Scripts/Game/Block.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Block : MonoBehaviour
+public class Block : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [field: SerializeField] public Pocket AttachedPocket { get; set; }
+
+    private Vector2 _startPosition;
+    private CanvasGroup _canvasGroup;
+
+    private void Start()
     {
-        
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        _canvasGroup.blocksRaycasts = false;
+        _startPosition = GetComponent<RectTransform>().anchoredPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = true;
+        GetComponent<RectTransform>().anchoredPosition = _startPosition;
     }
 }
--- Assets/Scripts/Game/Pocket.cs
+++ Assets/Scripts/Game/Pocket.cs
@@ -1,19 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Pocket : MonoBehaviour
+public class Pocket : MonoBehaviour, IDropHandler
 {
     [SerializeField] private Block attachedBlock;
-    
-    void Start()
+    [SerializeField] private bool containBlockOnInit;
+    [SerializeField] private GameObject blockPrefab;
+
+    private void Start()
     {
-        
+        if (containBlockOnInit && attachedBlock == null)
+        {
+            GameObject newBlock = Instantiate(blockPrefab, transform, false);
+            AttachBlock(newBlock.GetComponent<Block>());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AttachBlock(Block block)
     {
-        
+        if (block.AttachedPocket != null)
+        {
+            block.AttachedPocket.attachedBlock = null;
+        }
+
+        attachedBlock = block;
+        block.transform.SetParent(transform, false);
+        block.AttachedPocket = this;
+        block.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Block newBlock = eventData.pointerDrag.GetComponent<Block>();
+        if (newBlock == null)
+        {
+            Debug.LogError("On drop block wasn't found");
+            return;
+        }
+
+        if (CanTakeBlock())
+        {
+            AttachBlock(newBlock);
+        }
+    }
+
+    private bool CanTakeBlock()
+    {
+        return attachedBlock == null;
     }
 }
