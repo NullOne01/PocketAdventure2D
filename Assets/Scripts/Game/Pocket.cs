a34027828e90cf500251cb058ee7d2fd06a4029a
NullOne01 <NullOne01@users.noreply.github.com>
2022-10-09 04:38:54 +0300

    Added window 2

---
+++ Assets/Scripts/Game/Pocket.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocket : MonoBehaviour
+{
+    [SerializeField] private Block attachedBlock;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
